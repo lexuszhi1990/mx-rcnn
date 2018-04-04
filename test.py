cb056cfb54f60af05b256b08db17d4f8dd97fb8a
david <lexuszhi1990@gmail.com>
2018-04-04 16:31:47 +0800

    add test files

--- test.py
+++ test.py
@@ -18,24 +18,26 @@
 import argparse
 import mxnet as mx
 from rcnn.logger import logger
-from rcnn.config import config, default, generate_config
+from rcnn.config import config, generate_config
 from rcnn.tools.test_rcnn import test_rcnn
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test a Faster R-CNN network')
     # general
-    parser.add_argument('--network', help='network name', default=default.network, type=str)
-    parser.add_argument('--dataset', help='dataset name', default=default.dataset, type=str)
+    parser.add_argument('--network', help='network name', type=str)
+    parser.add_argument('--dataset', help='dataset name', type=str)
     args, rest = parser.parse_known_args()
+
     generate_config(args.network, args.dataset)
-    parser.add_argument('--image_set', help='image_set name', default=default.test_image_set, type=str)
-    parser.add_argument('--root_path', help='output data folder', default=default.root_path, type=str)
-    parser.add_argument('--dataset_path', help='dataset path', default=default.dataset_path, type=str)
+
+    parser.add_argument('--image_set', help='image_set name', default=config.test_image_set, type=str)
+    parser.add_argument('--root_path', help='output data folder', default=config.root_path, type=str)
+    parser.add_argument('--dataset_path', help='dataset path', default=config.dataset_path, type=str)
     # testing
-    parser.add_argument('--prefix', help='model to test with', default=default.e2e_prefix, type=str)
-    parser.add_argument('--epoch', help='model to test with', default=default.e2e_epoch, type=int)
-    parser.add_argument('--gpu', help='GPU device to test with', default=0, type=int)
+    parser.add_argument('--prefix', help='model to test with', default=config.e2e_prefix, type=str)
+    parser.add_argument('--epoch', help='model to test with', default=config.e2e_epoch, type=int)
+    parser.add_argument('--gpus', help='GPU device to test with', default=0, type=int)
     # rcnn
     parser.add_argument('--vis', help='turn on visualization', action='store_true')
     parser.add_argument('--thresh', help='valid detection threshold', default=1e-3, type=float)
@@ -49,7 +51,8 @@ def parse_args():
 def main():
     args = parse_args()
     logger.info('Called with argument: %s' % args)
-    ctx = mx.gpu(args.gpu)
+    ctx = mx.gpu(args.gpus)
+
     test_rcnn(args.network, args.dataset, args.image_set, args.root_path, args.dataset_path,
               ctx, args.prefix, args.epoch,
               args.vis, args.shuffle, args.has_rpn, args.proposal, args.thresh)
--- test/fashionai.py
+++ test/fashionai.py
@@ -5,7 +5,6 @@ from rcnn.dataset import FashionKeypoint
 fk = FashionKeypoint('None', root_path='/mnt/warm_up_trin/build', data_path='/mnt/warm_up_trin', is_dev=True)
 fk.gt_roidb()
 
-
 # legancy
 # if category == 'blouse':
 #     anno_keys = ['neckline_left', 'neckline_right', 'center_front', 'shoulder_left', 'shoulder_right', 'armpit_left', 'armpit_right', 'cuff_left_in', 'cuff_left_out', 'cuff_right_in', 'cuff_right_out', 'top_hem_left', 'top_hem_right']
